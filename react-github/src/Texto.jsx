e7292bae1ab2d8ae8df3f020dec28a3c1129d21c
Rodrigo Cruz <rcruz@studiovisual.com.br>
2025-07-28 14:14:25 -0300

    componente texto

---
+++ react-github/src/Texto.jsx
@@ -0,0 +1,11 @@
+function Texto() {
+    return (
+        <>
+            <div>
+                <h1>Olá aqui é apenas um texto</h1>
+            </div>
+        </>
+    )
+}
+
+export default Texto
